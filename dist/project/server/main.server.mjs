6553a6c87b028f9ef2690ed289dad9f0b27d97a9
Tetiana <80596978+Tetiana2304@users.noreply.github.com>
2024-01-17 15:32:05 +0200

    Add dist directory for GitHub Pages

---
+++ dist/project/server/main.server.mjs
@@ -0,0 +1,3 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-SW7V6F3W.mjs";import"./chunk-ITFQMCA3.mjs";import"./chunk-4S7LW3SP.mjs";export{a as default};
+//# sourceMappingURL=main.server.mjs.map
