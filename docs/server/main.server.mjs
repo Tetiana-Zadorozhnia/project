e88cb9ebf7e72a336ca3db85285d0407f38783f2
Tetiana <80596978+Tetiana2304@users.noreply.github.com>
2024-01-18 10:52:05 +0200

    d

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,3 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-BNFJOULB.mjs";import"./chunk-6U3SA4PG.mjs";import"./chunk-VAPKEZCW.mjs";export{a as default};
+//# sourceMappingURL=main.server.mjs.map
