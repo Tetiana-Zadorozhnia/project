9ece653d8381ffa5311a291d53c0e1ba104f9b1d
Tetiana <80596978+Tetiana2304@users.noreply.github.com>
2024-01-22 10:48:09 +0200

    improved adaptability

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,3 +1,3 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-JG4JFQZL.mjs";import"./chunk-6U3SA4PG.mjs";import"./chunk-VAPKEZCW.mjs";export{a as default};
+import{a}from"./chunk-TUGNTJOL.mjs";import"./chunk-6U3SA4PG.mjs";import"./chunk-VAPKEZCW.mjs";export{a as default};
 //# sourceMappingURL=main.server.mjs.map
