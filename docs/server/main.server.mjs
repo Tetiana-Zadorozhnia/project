e29334742350b93edc569ec01e5e85f93652239e
Tetiana <80596978+Tetiana2304@users.noreply.github.com>
2024-01-26 10:51:13 +0200

    font modification

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,3 +1,3 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-3HNNYIOU.mjs";import"./chunk-6U3SA4PG.mjs";import"./chunk-VAPKEZCW.mjs";export{a as default};
+import{a}from"./chunk-XSW7SWIJ.mjs";import"./chunk-6U3SA4PG.mjs";import"./chunk-VAPKEZCW.mjs";export{a as default};
 //# sourceMappingURL=main.server.mjs.map
